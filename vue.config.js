d33ac9457c4324f88a06786ff281270bf4a38b52
ttrommlitz <ttrommlitz6@gmail.com>
2022-10-06 08:20:45 -0600

    commit again

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/full-stack-project/'
-    : '/'
-}
+module.exports = defineConfig({
+  transpileDependencies: true
+})
