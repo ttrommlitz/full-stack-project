89b4f88982262131611f4fa57219fcd948ccd421
ttrommlitz <ttrommlitz6@gmail.com>
2022-10-21 09:45:36 -0600

    again

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.use(cors({
     methods: ['GET','POST','DELETE','PUT',]
 }))
 
-app.use(express.static(path.join(__dirname, "./dist")))
+app.use(express.static(__dirname + './dist'))
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist', 'index.html'))
+    res.sendFile(__dirname + './dist/index.html')
 })
 
 //Mongo connection and setup
