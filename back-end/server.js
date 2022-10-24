9b5322a5887a4d73f80ab442df87c92760043abf
Emmanuel Rivera <manny.rivera@byu.edu>
2022-10-24 14:51:10 -0600

    update: wip.

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const mongoose = require('mongoose')
 const toDoRoutes = require('./routes/toDoRoutes')
 
 const app = express();
@@ -28,15 +27,6 @@ app.use(cors({
     origin: '*',
     methods: ['GET','POST','DELETE','PUT',]
 }))
-
-// app.use(express.static(__dirname + '/dist'))
-// app.get('/*', (req, res) => {
-//     res.sendFile(__dirname + '/dist/index.html')
-// })
-
-//Mongo connection and setup
-const url = 'mongodb+srv://ttrommlitz:Soccer03%21@todo-storage.c4i7nan.mongodb.net/test'
-mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
 
 //CRUD endpoints
 app.use('/api/todo', toDoRoutes)
